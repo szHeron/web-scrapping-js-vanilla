c8519bb512c04b934995d75d74defd47c2b78991
szheron <heronbr7@gmail.com>
2024-02-26 16:15:15 -0300

    refactor: Renamed variables, modularity is improved

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 const food = {}
 
-function Start(){
-    food.informations = GetBasicInformations()
-    food.scores = GetScore()
-    food.ingredients = GetIngredients()
-    food.nutrition = GetNutrition()
+function start(){
+    food.informations = getBasicInformations()
+    food.scores = getScore()
+    food.ingredients = getIngredients()
+    food.nutrition = getNutrition()
     console.log(food)
 }
 
-function GetBasicInformations(){
-    const informations = {}
-    const title = document.querySelector('.title-1')
-    const barcode = document.querySelector('#barcode')
-    const image = document.querySelector('#og_image')
-    const restOfinformationNamesForScrapping = ['brands', 'countries', 'quantity', 'categories']
-    const informationElements = [...document.querySelectorAll('.field_value')].map(item => {
+function getBasicInformations(){
+    const titleElement = document.querySelector('.title-1')
+    const barcodeElement = document.querySelector('#barcode')
+    const imageElement = document.querySelector('#og_image')
+    const informationElements = [...document.querySelectorAll('.field_value')]
+    const informationsMap = informationElements.map(item => {
         return {
             informationName: item.id.match(/_(.+)_/)?.[1],
             informationText: item.innerText
         }
     })
+    const restOfinformationNames = ['brands', 'countries', 'quantity', 'categories']
+    const restOfInformations = {}
 
-    restOfinformationNamesForScrapping.forEach(name => {
-        const info = informationElements.find(item => item.informationName === name)
-        informations[name] = info?info.informationText:'Unknown'
+    restOfinformationNames.forEach(name => {
+        const info = informationsMap.find(item => item.informationName === name)
+        restOfInformations[name] = info?info.informationText:'Unknown'
     })
 
-    informations.title = title?title.innerText:'Unknown'
-    informations.barcode = barcode?barcode.innerText:'Unknown'
-    informations.image = image?image.src:'Unknown'
-
-    return informations
+    return {
+        title: titleElement?titleElement.innerText:'Unknown',
+        barcode: barcodeElement?barcodeElement.innerText:'Unknown',
+        image: imageElement?imageElement.src:'Unknown',
+        ...restOfInformations
+    }
 }
 
-function GetScore(){
+function getScore(){
     const scores = {}
     const scoreNamesForScrapping = ['nutriscore', 'nova', 'ecoscore']
 
@@ -52,24 +54,24 @@ function GetScore(){
 }
 
 
-function GetIngredients(){
+function getIngredients(){
     const ingredients = {
         hasPalmOil: 'Unknown',
         isVegan: 'Unknown',
         isVegetarian: 'Unknown',
         list: ''
     }
-    const ingredientAnalysisElements = [...document.querySelectorAll('a[href*="#panel_ingredients_analysis"]')].map(item => item.querySelector('h4').innerText)
+    const listOfAnalyzedIngredients = [...document.querySelectorAll('a[href*="#panel_ingredients_analysis"]')].map(item => item.querySelector('h4').innerText)
 
-    if(ingredientAnalysisElements.length > 0){
-        ingredientAnalysisElements.slice(0,3).forEach(item => {
+    if(listOfAnalyzedIngredients.length > 0){
+        listOfAnalyzedIngredients.slice(0,3).forEach(item => {
             item = item.toLocaleUpperCase()
             if(item.includes('VEGANO')){
-                ingredients.isVegan = IngredientAnalysis(item)
+                ingredients.isVegan = analyzeIngredient(item)
             }else if(item.includes('VEGETARIANO')){
-                ingredients.isVegetarian = IngredientAnalysis(item)
+                ingredients.isVegetarian = analyzeIngredient(item)
             }else{
-                ingredients.hasPalmOil = IngredientAnalysis(item)
+                ingredients.hasPalmOil = analyzeIngredient(item)
             }
         })
 
@@ -79,7 +81,7 @@ function GetIngredients(){
     return ingredients
 }
 
-function IngredientAnalysis(ingredient){
+function analyzeIngredient(ingredient){
     if(['NÃO', 'POSSIVELMENTE', 'PODE'].some(word => ingredient.includes(word))){
         return false
     }else if(['DESCONHECIDO', 'DESCONHECE-SE'].some(word => ingredient.includes(word))){
@@ -89,10 +91,10 @@ function IngredientAnalysis(ingredient){
     }
 }
 
-function GetNutrition(){
+function getNutrition(){
     const nutrition = {}
     const nutritionValuesElement = document.querySelector('#panel_nutrient_levels_content')
-    const servingSize = document.querySelector('#panel_serving_size_content')
+    const servingSizeElement = document.querySelector('#panel_serving_size_content')
 
     if(nutritionValuesElement){
         nutrition.nutritionValues = [...nutritionValuesElement.querySelectorAll('h4')].map(item => item.innerText)
@@ -100,17 +102,17 @@ function GetNutrition(){
         nutrition.nutritionValues = 'Unknown'
     }
 
-    nutrition.nutritionData = GetNutritionDataTable()
-    nutrition.servingSize = servingSize?servingSize.innerText:'Unknown'
+    nutrition.nutritionData = getNutritionDataTable()
+    nutrition.servingSize = servingSizeElement?servingSizeElement.innerText:'Unknown'
 
     return nutrition
 }
 
-function GetNutritionDataTable(){
+function getNutritionDataTable(){
     const nutritionData = {}
     const tableBodyElement = [...document.querySelector('[aria-label="Dados nutricionais"]').querySelectorAll('tr')]
 
-    tableBodyElement.slice(1,tableBodyElement.length).forEach(tr => {
+    tableBodyElement.slice(1).forEach(tr => {
         const data = [...tr.querySelectorAll('td')].map(item => item.innerText)
         const per100g = data[1].length > 1?data[1].replace('\n', ''):'Unknown'
         const perServing = data[2].length > 1?data[2].replace('\n', ''):'Unknown'
@@ -120,8 +122,8 @@ function GetNutritionDataTable(){
             perServing
         }
     })
-    
+
     return nutritionData
 }
 
-Start()
+start()
